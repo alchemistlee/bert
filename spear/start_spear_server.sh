154fee4013f443c0cc3be17b4389c695fc2419ac
alchemistlee <bingyanlyf@gmail.com>
2019-06-05 18:49:40 +0800

    up

--- spear/start_spear_server.sh
+++ spear/start_spear_server.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-python3 spear_server.py --host 0.0.0.0 --port 10003 --authkey qweasd
+#1. 启动后台服务
+
+nohup python3 spear_server.py --host 0.0.0.0 --port 10003 --authkey qweasd &
+
+#2. 启动服务接口
+
+nohup python3 spear_interface.py > web.log 2>&1 &
+
+
